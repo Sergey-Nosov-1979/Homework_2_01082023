abd8d0ab889e988515b59133d2ecce3790401cbf
Sergey-Nosov-1979 <138179653+Sergey-Nosov-1979@users.noreply.github.com>
2023-08-02 15:01:43 +0900

    Add files via upload

--- task_12.py
+++ task_12.py
@@ -7,21 +7,21 @@ X и Y (X,Y≤1000), а Катя должна их отгадать. Для эт
 
 x = abs(int(input('Введите первое натуральное число X от 1 до 1000 ')))
 y = abs(int(input('Введите второе натуральное число Y от 1 до 1000 ')))
-if x < 1 or x > 1000 or y < 1 or y > 1000:
+if x < 1 or x > 10 or y < 1 or y > 10:
     print('Вы ввели числа не из заданного диапазона - начните сначало!')
 else:
     Addition = x + y
     Multiplication = x * y
     stop = 0
-    for i in range(1001):
-        if stop != 1:
-            for j in range(1001):
-                if stop != 1:
+    for i in range(11):
+        if stop != 1 and i > 0:
+            for j in range(11):
+                if stop != 1 and  j > 0:
                     if i * j == Multiplication and i + j == Addition:
                         print((f'Одно из загаданных чисел - {i}'))
                         print((f'Второе из загаданных чисел - {j}'))
                         stop = 1
-            else:
-                j = 1001
+                else:
+                    j = 11
         else:
-            i = 1001
+            i = 11
